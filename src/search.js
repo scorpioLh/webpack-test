53f4661dfad8540ee1a1a471d5165f2800260bad
linhan <scorpio360@163.com>
2019-06-02 21:20:19 +0800

    webpack解析react

--- src/search.js
+++ src/search.js
@@ -1,1 +1,15 @@
-document.write('search page')
+'use strict'
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class Search extends React.Component{
+  render() {
+    return <div>Search Test</div>
+  }
+} 
+
+ReactDom.render(
+  <Search />,
+  document.getElementById('root')
+)
