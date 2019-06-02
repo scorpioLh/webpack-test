a5118442cb8498fa3f6bbd2d63f0e677b9e9b34e
linhan <scorpio360@163.com>
2019-06-02 21:05:57 +0800

    submit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
