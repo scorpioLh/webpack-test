7c3608824fd67a34524d1cc63fca533f87e1c153
linhan <scorpio360@163.com>
2019-06-17 23:32:39 +0800

    使用postcss-loader,autoprefixer给css添加前缀，提升兼容性

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,7 +34,17 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'less-loader'
+                    'less-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [
+                                require('autoprefixer') ({
+                                    overrideBrowserslist: ['last 2 version', '>1%', 'ios 7']
+                                })
+                            ]
+                        }
+                    }
                 ]
             },
             {
