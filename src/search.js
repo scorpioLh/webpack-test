0283f4c2a94d6fe45a7ca36fb6d1575007e2d86e
linhan <scorpio360@163.com>
2019-06-17 22:52:46 +0800

    copy

--- src/search.js
+++ src/search.js
@@ -1,17 +1,20 @@
-'use strict'
+'use strict';
 
-import React from 'react'
-import ReactDom from 'react-dom'
-import './search.less'
-import logo from './images/test.jpg'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './images/logo.png';
+import './search.less';
 
-class Search extends React.Component{
-  render() {
-    return <div className="search-text">Search Test333<img src={ logo } /></div>
-  }
-} 
+class Search extends React.Component {
 
-ReactDom.render(
-  <Search />,
-  document.getElementById('root')
-)
+    render() {
+        return <div className="search-text">
+            搜索文字的内容<img src={ logo } />
+        </div>;
+    }
+}
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+);
